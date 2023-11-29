fc6fa2a4184f5a7bc0ecb2568cf3bbd9fdc75e02
AlGAHBOUZ <75996768+AlGAHBOUZ@users.noreply.github.com>
2023-11-29 20:35:43 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const sql = require('mssql');
+const { Connection } = require('tedious');
+const app = express();
+const port = 3000;
+
+const config = {
+  server: 'localhost',
+  database: 'Clinic_Database',
+  user: 'sa',
+  password: 'yourStrong(!)Password',
+  options: {
+    trustedConnection: false,
+    enableArithAbort: true,
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+const connection = new Connection(config);
+
+connection.on('connect', (err) => {
+  if (err) {
+    console.error('Error connecting to SQL Server:', err.message);
+  } else {
+    console.log('Connected to SQL Server');
+  }
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.post('/add-patient', async (req, res) => {
+  const { name, phone, age, branch, diagnosis, gender, generalNotes, sessionNotes } = req.body;
+
+  try {
+    await sql.connect(config);
+
+    const result = await sql.query`INSERT INTO Patients (patient_name, phone_number, age, branch, diagnosis, gender, general_notes, session_notes)
+    VALUES(${name}, ${phone}, ${age}, ${branch}, ${diagnosis}, ${gender}, ${generalNotes}, ${sessionNotes})`;
+
+    res.redirect('/add-patient.html?success=true'); // Redirect to add-patient.html with success=true
+  } catch (error) {
+    console.error('Error inserting data:', error.message);
+    res.send('Error inserting data');
+  } finally {
+    await sql.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
