f42926a0c1ee13984fe09f4ab4968bfd9750c96d
AlGAHBOUZ <75996768+AlGAHBOUZ@users.noreply.github.com>
2023-12-22 21:57:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// app.js
 const express = require('express');
 const sql = require('mssql');
 const { Connection } = require('tedious');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -29,16 +30,19 @@ connection.on('connect', (err) => {
 });
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // Add this line to parse JSON-encoded bodies
 app.use(express.static('public'));
 
 app.post('/add-patient', async (req, res) => {
-    const { name, phone, age, branch, diagnosis, gender, generalNotes, sessionNotes } = req.body;
+    const { name, phone, age, branch, diagnosis, gender, generalNotes, sessionNotes, doctorId, date } = req.body;
 
     try {
         await sql.connect(config);
 
-        const result = await sql.query`INSERT INTO Patients (patient_name, phone_number, age, branch, diagnosis, gender, general_notes, session_notes)
-        VALUES(${name}, ${phone}, ${age}, ${branch}, ${diagnosis}, ${gender}, ${generalNotes}, ${sessionNotes})`;
+        const result = await sql.query`
+            INSERT INTO Patients (patient_name, PhoneNumber, age, gender, general_notes, session_notes, DoctorID, time_stamp, BranchName, diagnosis)
+            VALUES(${name}, ${phone}, ${age}, ${gender}, ${generalNotes}, ${sessionNotes}, ${doctorId}, ${date}, ${branch}, ${diagnosis})
+        `;
 
         res.redirect('/add-patient.html?success=true'); // Redirect to add-patient.html with success=true
     } catch (error) {
@@ -49,18 +53,69 @@ app.post('/add-patient', async (req, res) => {
     }
 });
 
-app.get('/search-patient', async (req, res) => {
-    const name = req.query.name;
+app.post('/search-patient', async (req, res) => {
+    console.log('Request Body:', req.body);
+
+    const { name, phone, ageFrom, ageTo, gender, branch, diagnosis, dateFrom, dateTo } = req.body;
 
     try {
         await sql.connect(config);
 
-        const result = await sql.query`EXEC select_patient @name = ${name}`;
+        const result = await sql.query`EXEC filter_patient 
+            @name = ${name || null},
+            @PhoneNumber = ${phone || null},  
+            @age_from = ${ageFrom || null},
+            @age_to = ${ageTo || null},
+            @gender = ${gender || null},
+            @branch = ${branch || null},
+            @diagnosis = ${diagnosis || null},
+            @date_from = ${dateFrom || null},
+            @date_to = ${dateTo || null}`;
+
         const patients = result.recordset;
+
+        console.log('Patients Returned:', patients);  // Log the patients data
 
         res.json(patients);
     } catch (error) {
         console.error('Error searching for patients:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await sql.close();
+    }
+});
+
+
+
+app.get('/generate-reports', async (req, res) => {
+    try {
+        await sql.connect(config);
+
+        const result = await sql.query`EXEC select_all`;
+        const patients = result.recordset;
+
+        const doc = new PDFDocument();
+
+        // Customize your PDF content here based on the patient data
+        for (const patient of patients) {
+            doc.fontSize(16).text(`Patient Name: ${patient.patient_name}`);
+            doc.fontSize(16).text(`Phone Number: ${patient.PhoneNumber}`);
+            // Add more patient information as needed
+            doc.moveDown();
+        }
+
+        // End the PDF document
+        doc.end();
+
+        // Set the response headers for PDF download
+        res.setHeader('Content-disposition', 'attachment; filename=report.pdf');
+        res.setHeader('Content-type', 'application/pdf');
+
+        // Pipe the PDF content directly to the response
+        doc.pipe(res);
+
+    } catch (error) {
+        console.error('Error generating report:', error.message);
         res.status(500).send('Internal Server Error');
     } finally {
         await sql.close();
---
+++ public/test.sql
@@ -0,0 +1,66 @@
+-- Use the newly created database
+USE Clinic_Database;
+GO
+
+-- Create a table for patient information
+CREATE TABLE Patients (
+    PatientID INT IDENTITY(1,1) PRIMARY KEY,
+    patient_name VARCHAR(255),
+    age INT,
+    branch VARCHAR(50),
+    diagnosis VARCHAR(50),
+    gender VARCHAR(10),
+    general_notes TEXT,
+    session_notes TEXT
+);
+
+CREATE TABLE Patient_Numbers(
+
+    PatientID INT IDENTITY(1,1) PRIMARY KEY,
+    PhoneNumber NVARCHAR(50)
+)
+
+
+-- Create a table for employees
+CREATE TABLE Employees (
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    salary_type VARCHAR(50),
+    salary_amount DECIMAL(10, 2),
+    total_salary DECIMAL(10, 2),
+    notes TEXT
+);
+
+CREATE TABLE Employee_Numbers(
+
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    PhoneNumber NVARCHAR(50)
+)
+
+-- Create a junction table for the many-to-many relationship between Employees and Patients
+
+
+-- Create a table for sessions
+CREATE TABLE Sessions (
+    SessionID INT IDENTITY(1,1) PRIMARY KEY, 
+    Type NVARCHAR(100) NOT NULL,
+    Price INT NOT NULL
+);
+
+-- Create a table to track sessions for patients
+CREATE TABLE Patient_Sessions (
+    PatientSessionID INT IDENTITY(1,1) PRIMARY KEY,
+    PatientID INT,
+    SessionID INT,
+    NurseID INT,
+    Quantity INT,
+    Notes TEXT,
+    SessionDate DATE,
+    PaidThisSession DECIMAL(10, 2),
+    PaidOverall DECIMAL(10, 2),
+    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
+    FOREIGN KEY (SessionID) REFERENCES Sessions(SessionID),
+    FOREIGN KEY (NurseID) REFERENCES Employees(EmployeeID)
+);
+
